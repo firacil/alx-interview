a88d88450ef4df402ce0f4d20caf18f910c9c87f
Firaol Mk <firaolmekbib511@gmail.com>
2024-07-26 03:06:00 +0300

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script to solve pascal's triangle for any number"""
+
+
+def pascal_triangle(n):
+    """
+    returns list of list of integers representing the pascal triangles of a number
+    """
+    triangle = []
+    
+    # return (triangle if n <= 0)
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        templ = []
+        
+        for j in range(i+1):
+            if j == 0 or j == 1:
+                templ.append(1)
+            else:
+                templ.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(templ)
+    # return (triangle)
+    return triangle
+                
