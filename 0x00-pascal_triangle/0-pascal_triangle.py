3048d84f7b98d5b53cf0546d4230445069712105
Firaol Mk <firaolmekbib511@gmail.com>
2024-07-26 03:18:44 +0300

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,14 +12,13 @@ def pascal_triangle(n):
     # return (triangle if n <= 0)
     if n <= 0:
         return triangle
-    for i in range(n):
-        templ = []
-
-        for j in range(i+1):
-            if j == 0 or j == 1:
-                templ.append(1)
-            else:
-                templ.append(triangle[i-1][j-1] + triangle[i-1][j])
+    triangle = [[1]]
+    for i in range(1, n):
+        templ = [1]
+        for j in range(len(triangle[i - 1]) - 1):
+            curr = triangle[i - 1]
+            templ.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+        templ.append(1)
         triangle.append(templ)
     # return (triangle)
     return triangle
