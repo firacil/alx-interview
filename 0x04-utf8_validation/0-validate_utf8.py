8eca843454fb51e4b87d6901a57baa4a06edc68d
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-08-22 08:56:52 +0000

    utf-validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+	a method that determines if a given data set
+	represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+	"""determines if a given data set represents
+	a valid UTF-8 encoding.
+	"""
+	for num in data:
